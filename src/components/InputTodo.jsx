05f1d435a1ae8fab3c2fdfa29e30a9e04c923e3e
Fresinsya <vicafresinsya@gmail.com>
2023-10-28 13:25:35 +0700

    tampilan

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -4,10 +4,12 @@ const InputTodo = () => {
     return (
         <div>
             <h1 className='text-3xl text-center font-bold '>Todo List</h1>
-            <input type="text" placeholder='Tambahkan Todo'
-                className='border-2 items-center justify-center border-gray-400 rounded-xl p-2 m-3'
-            ></input>
-            <button type='submit' className='p-3 rounded-2xl' style={{backgroundColor: '#113946', color: 'white'}}>add</button>
+            <div className='flex justify-center items-center'>
+                <input type="text" placeholder='Tambahkan Todo'
+                    className='border-2 items-center justify-center border-gray-400 rounded-xl p-2 m-3'/>
+                <button type='submit' className='p-3 rounded-2xl h-12' style={{ backgroundColor: '#113946', color: 'white' }}>add</button>
+
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import InputTodo from '../components/InputTodo'
 import ListTodo from '../components/ListTodo'
 
 const Home = () => {
-  return (
-    <div style={{backgroundColor: '#FFF2D8'}} className='h-screen'>
-        <InputTodo />
-        <ListTodo />
-    </div>
-  )
+    return (
+        <div style={{ backgroundColor: '#FFF2D8' }} className='h-screen flex justify-center items-center'>
+            <div className='border border-4 rounded-2xl w-5/6 border-green-700'>
+                <InputTodo />
+                <ListTodo />
+            </div>
+        </div>
+    )
 }
 
 export default Home
